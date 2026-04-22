4a18b4f33d7121f581636e66c3762a2499dce7d2
Nuzulia S.M. <nuzuliasm@gmail.com>
2026-04-22 20:28:39 +0800

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -12,60 +12,39 @@ bool isEmpty(const Queue* q) {
 
 bool isFull(const Queue* q) {
     if (isEmpty(q)) return false;
-    int* next_rear;
-    if (q->rear == q->data + MAX - 1) {
-        next_rear = q->data;
-    } else {
-        next_rear = q->rear + 1;
-    }
-
+    int* next_rear = q->data + ((q->rear - q->data + 1) % MAX);
     return next_rear == q->front;
 }
 
 void enqueue(Queue* q, int value) {
-    if (isFull(q)) {
-        throw std::overflow_error("Queue is full");
-    }
-
+    if (isFull(q)) throw std::overflow_error("Queue is full");
+    
     if (isEmpty(q)) {
         q->front = q->data;
         q->rear = q->data;
     } else {
-        if (q->rear == q->data + MAX - 1) {
-            q->rear = q->data;
-        } else {
-            q->rear++;
-        }
+        q->rear = q->data + ((q->rear - q->data + 1) % MAX);
     }
     *(q->rear) = value;
 }
 
 void dequeue(Queue* q) {
-    if (isEmpty(q)) {
-        throw std::underflow_error("Queue is empty");
-    }
-
+    if (isEmpty(q)) throw std::underflow_error("Queue is empty");
+    
     if (q->front == q->rear) {
-        init(q);
+        q->front = nullptr;
+        q->rear = nullptr;
     } else {
-        if (q->front == q->data + MAX - 1) {
-            q->front = q->data;
-        } else {
-            q->front++;
-        }
+        q->front = q->data + ((q->front - q->data + 1) % MAX);
     }
 }
 
 int front(const Queue* q) {
-    if (isEmpty(q)) {
-        throw std::underflow_error("Queue is empty");
-    }
+    if (isEmpty(q)) throw std::underflow_error("Queue is empty");
     return *(q->front);
 }
 
 int back(const Queue* q) {
-    if (isEmpty(q)) {
-        throw std::underflow_error("Queue is empty");
-    }
+    if (isEmpty(q)) throw std::underflow_error("Queue is empty");
     return *(q->rear);
 }

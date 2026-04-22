becc65340e495d4ecfe1eeded3357fe63d97ebbf
Nuzulia S.M. <nuzuliasm@gmail.com>
2026-04-22 19:08:35 +0800

    stack implementation

--- stack.cpp
+++ stack.cpp
@@ -1,40 +1,32 @@
 #include "stack.h"
-#include <stdexcept>
 
 void init(Stack* s) {
-    s->top = nullptr; 
+    s->top = s->data - 1;
 }
 
 bool isEmpty(const Stack* s) {
-    return s->top == nullptr;
+    return s->top == s->data - 1;
 }
 
 bool isFull(const Stack* s) {
-    if (isEmpty(s)) return false;
-    return s->top == &(s->data[MAX - 1]);
+    return s->top == s->data + MAX - 1;
 }
 
 void push(Stack* s, int value) {
-    if (isFull(s)) throw std::runtime_error("Stack Overflow");
-    
-    if (isEmpty(s)) {
-        s->top = &(s->data[0]);
-    } else {
-        s->top++;
+    if (!isFull(s)) {
+        *(++s->top) = value;
     }
-    *(s->top) = value;
 }
 
 void pop(Stack* s) {
-    if (isEmpty(s)) throw std::runtime_error("Stack Underflow");
-    if (s->top == &(s->data[0])) {
-        s->top = nullptr;
-    } else {
+    if (!isEmpty(s)) {
         s->top--;
     }
 }
 
 int peek(const Stack* s) {
-    if (isEmpty(s)) throw std::runtime_error("Stack Kosong");
-    return *(s->top);
+    if (!isEmpty(s)) {
+        return *s->top;
+    }
+    return -1;
 }

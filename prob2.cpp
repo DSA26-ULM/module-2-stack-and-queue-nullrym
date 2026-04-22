dbd9bae3e36db2d8d40e834b090d62d5dfbb8fdc
Nuzulia S.M. <nuzuliasm@gmail.com>
2026-04-22 20:36:15 +0800

    Update prob2.cpp

--- prob2.cpp
+++ prob2.cpp
@@ -6,32 +6,30 @@ using namespace std;
 int main() {
     int n, k;
     if (!(cin >> n >> k)) return 0;
-
     int A[105];
-    for (int i = 0; i < n; i++) cin >> A[i];
-
-    if (k <= 0) {
-        for (int i = 0; i < n; i++) cout << 0 << (i == n-1 ? "" : " ");
-        cout << endl;
-        return 0;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
     }
 
     Queue q;
     init(&q);
-    long long total_sekarang = 0;
+    
+    int total = 0;
 
     for (int i = 0; i < n; i++) {
         enqueue(&q, A[i]);
-        total_sekarang += A[i];
+        total += A[i];
 
         if (i >= k) {
-            total_sekarang -= front(&q);
+            total -= front(&q);
             dequeue(&q);
         }
 
         if (i >= k - 1) {
-            cout << total_sekarang;
-            if (i < n - 1) cout << " ";
+            cout << total;
+            if (i < n - 1) {
+                cout << " ";
+            }
         }
     }
     cout << endl;

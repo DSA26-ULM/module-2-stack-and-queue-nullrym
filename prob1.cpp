42a3cf992ae39c44fc53b3990cef01d60f451b1b
Nuzulia S.M. <nuzuliasm@gmail.com>
2026-04-22 19:56:39 +0800

    Update prob1.cpp

--- prob1.cpp
+++ prob1.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> // Wajib pakai cstdlib untuk atoi()
-#include "stack.h" // Tetap gunakan stack.h, jangan diubah
+#include <cctype>
+#include "stack.h"
 
 using namespace std;
 
 int main() {
     int n;
-    // Baca jumlah n simbol
     if (!(cin >> n)) return 0;
 
     Stack s;
@@ -17,32 +16,18 @@ int main() {
         string token;
         cin >> token;
 
-        // Cek langsung apakah token adalah operator
-        if (token == "+") {
-            int val2 = peek(&s); pop(&s);
-            int val1 = peek(&s); pop(&s);
-            push(&s, val1 + val2);
-        } else if (token == "-") {
-            int val2 = peek(&s); pop(&s);
-            int val1 = peek(&s); pop(&s);
-            push(&s, val1 - val2);
-        } else if (token == "*") {
-            int val2 = peek(&s); pop(&s);
-            int val1 = peek(&s); pop(&s);
-            push(&s, val1 * val2);
-        } else if (token == "/") {
-            int val2 = peek(&s); pop(&s);
-            int val1 = peek(&s); pop(&s);
-            push(&s, val1 / val2);
+        if (isdigit(token[0]) || (token.length() > 1 && token[0] == '-')) {
+            push(&s, stoi(token));
         } else {
-            // Jika bukan salah satu operator di atas, pasti dia angka.
-            // Gunakan atoi() yang lebih kompatibel dengan auto-grader.
-            push(&s, atoi(token.c_str()));
+            int val2 = peek(&s); pop(&s);
+            int val1 = peek(&s); pop(&s);
+            if (token == "+") push(&s, val1 + val2);
+            else if (token == "-") push(&s, val1 - val2);
+            else if (token == "*") push(&s, val1 * val2);
+            else if (token == "/") push(&s, val1 / val2);
         }
     }
 
-    // Tampilkan hasil akhir
     cout << peek(&s) << endl;
-
     return 0;
 }

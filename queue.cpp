7a6c577bae88603e811d0f0510bd80d9cf639ce5
Nuzulia S.M. <nuzuliasm@gmail.com>
2026-04-22 19:01:32 +0800

    Implement queue operations in queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,1 +1,47 @@
 #include "queue.h"
+#include <stdexcept>
+
+void init(Queue* q) {
+    q->front = nullptr;
+    q->rear = nullptr;
+}
+
+bool isEmpty(const Queue* q) {
+    return q->front == nullptr;
+}
+
+bool isFull(const Queue* q) {
+    if (isEmpty(q)) return false;
+    return q->rear == &(q->data[MAX - 1]);
+}
+
+void enqueue(Queue* q, int value) {
+    if (isFull(q)) throw std::runtime_error("Queue Full");
+    
+    if (isEmpty(q)) {
+        q->front = &(q->data[0]);
+        q->rear = &(q->data[0]);
+    } else {
+        q->rear++;
+    }
+    *(q->rear) = value;
+}
+
+void dequeue(Queue* q) {
+    if (isEmpty(q)) throw std::runtime_error("Queue Empty");
+    if (q->front == q->rear) {
+        init(q);
+    } else {
+        q->front++;
+    }
+}
+
+int front(const Queue* q) {
+    if (isEmpty(q)) throw std::runtime_error("Queue Kosong");
+    return *(q->front);
+}
+
+int back(const Queue* q) {
+    if (isEmpty(q)) throw std::runtime_error("Queue Kosong");
+    return *(q->rear);
+}
